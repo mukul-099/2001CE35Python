822945666bb1d1efc254b70de7b35401bc089087
Mukul <panchalmukul99@gmail.com>
2022-08-27 19:35:02 +0530

    Completed tut 1

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,5 +1,15 @@
-def factorial(int x):
-    
+# Creating function for calculating factorial.
+def factorial(a):
+    if a==1:
+        return 1 #Case 1
+    elif a==0:
+        return 1 #Case 2
+    else:
+        return (a*factorial(a-1)) #Recursive function.
 
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+a = int(input('Enter the number: ')) #Taking input from user.
+if a<0: #Incase number is negetive.
+    print("Factorial for negetive numbers does not exists.")
+else:
+    ans=factorial(a) #Calling the function.
+    print("Factorial of ", a, " is: ", ans ) #Printing the final answer.
