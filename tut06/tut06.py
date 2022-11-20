5507de492545a70f006d1a64451d49467f109b50
Mukul <panchalmukul99@gmail.com>
2022-11-20 19:40:58 +0530

    Concatenating the given data into desired format and string in respective columns.
    Concatenating the given data into desired format and string in respective columns.

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -22,3 +22,27 @@ def AtTeNdAnCe_ReP():
     DaTaFrAmE1['Day']=''
     DaTaFrAmE1['Month']=''
     DaTaFrAmE1['Year']=''
+    
+    #Concatenating the given data into desired format and string in respective columns.
+    DaTaFrAmE1.loc[DaTaFrAmE1.Roll=='','Roll']=DaTaFrAmE1.Attendance.str.split().str.get(0)
+    DaTaFrAmE1.loc[DaTaFrAmE1.Time=='','Time']=DaTaFrAmE1.Timestamp.str.split().str.get(1)
+    DaTaFrAmE1.loc[DaTaFrAmE1.Date=='','Date']=DaTaFrAmE1.Timestamp.str.split().str.get(0)
+    DaTaFrAmE1.loc[DaTaFrAmE1.Day=='','Day']=DaTaFrAmE1.Date.str.split('-').str.get(0)
+    DaTaFrAmE1.loc[DaTaFrAmE1.Month=='','Month']=DaTaFrAmE1.Date.str.split('-').str.get(1)
+    DaTaFrAmE1.loc[DaTaFrAmE1.Year=='','Year']=DaTaFrAmE1.Date.str.split('-').str.get(2)
+
+    #An empty list to store the valid days (Modays and Thurdays)
+    VAL_DATES=[]
+    SaKuRa=''
+    #Indexing over the dataframe for getting valid dates, using 'datetime' library for the same.
+    for i in DaTaFrAmE1.index:
+        DATE = datetime(int(DaTaFrAmE1['Year'][i]),int(DaTaFrAmE1['Month'][i]),int(DaTaFrAmE1['Day'][i]))
+        if (DATE.weekday()==0 or DATE.weekday()==3):
+            if (SaKuRa!= DaTaFrAmE1['Date'][i]):
+                VAL_DATES.append(DaTaFrAmE1['Date'][i])
+                SaKuRa = DaTaFrAmE1['Date'][i]
+        else:
+            DaTaFrAmE1['Date'][i]=-1
+
+    #Sorting our processed dataframe according to roll numbers for the ease of evaluation.
+    DaTaFrAmE1=DaTaFrAmE1.sort_values('Roll')
