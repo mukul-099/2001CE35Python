b3a1d10c96d9657269353e0d990561622c52dc2a
Mukul <panchalmukul99@gmail.com>
2022-11-20 19:42:43 +0530

    Logic.
    Logic.

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -46,3 +46,77 @@ def AtTeNdAnCe_ReP():
 
     #Sorting our processed dataframe according to roll numbers for the ease of evaluation.
     DaTaFrAmE1=DaTaFrAmE1.sort_values('Roll')
+    
+    #Creating a dataframe to store to consolidated output.
+    FIN_DaTaFrAmE = pAnDa_jI.DataFrame()
+    FIN_DaTaFrAmE['Roll'] = ''
+    FIN_DaTaFrAmE['Name'] = ''
+    for i in range(0, len(VAL_DATES)):
+        FIN_DaTaFrAmE[VAL_DATES[i]] = ''
+    FIN_DaTaFrAmE['Actual Lecture Taken'] = ''
+    FIN_DaTaFrAmE['Total Real'] = ''
+    FIN_DaTaFrAmE['% Attendance'] = ''
+
+    #Giving structure according to our valid dates stored in the list.
+    for i in DaTaFrAmE2.index:
+        FIN_DaTaFrAmE.at[i+1, 'Roll'] = DaTaFrAmE2['Roll No'][i]
+        FIN_DaTaFrAmE.at[i+1, 'Name'] = DaTaFrAmE2['Name'][i]
+        FIN_DaTaFrAmE.at[i+1, 'Actual Lecture Taken'] = len(VAL_DATES)
+        FIN_DaTaFrAmE.at[i+1,
+                         'Total Real'] = FIN_DaTaFrAmE.at[i+1, '% Attendance'] = 0
+
+    try:
+        #Main Loop (iterating for each roll number one by one)
+        for i in DaTaFrAmE2.index:
+            #A variable dataframe which will store data for every roll number in each iteration.
+            DaTaFrAmE3 = pAnDa_jI.DataFrame()
+            DaTaFrAmE3['Date'] = ''
+            DaTaFrAmE3['Roll'] = ''
+            DaTaFrAmE3['Name'] = ''
+            DaTaFrAmE3['Total Attendance Count'] = 0
+            DaTaFrAmE3['Real'] = ''
+            DaTaFrAmE3['Duplicate'] = ''
+            DaTaFrAmE3['Invalid'] = ''
+            DaTaFrAmE3['Absent'] = ''
+
+            DaTaFrAmE3.at[0, 'Roll'] = DaTaFrAmE2['Roll No'][i]
+            DaTaFrAmE3.at[0, 'Name'] = DaTaFrAmE2['Name'][i]
+
+            #Writing valid dates in the dataframe from previously formed list.
+            for j in range(1, len(VAL_DATES)+1):
+                DaTaFrAmE3.at[j, 'Date'] = VAL_DATES[j-1]
+                DaTaFrAmE3.at[j, 'Total Attendance Count'] = DaTaFrAmE3.at[j, 'Real'] = DaTaFrAmE3.at[j,
+                                                                                                      'Duplicate'] = DaTaFrAmE3.at[j, 'Invalid'] = DaTaFrAmE3.at[j, 'Absent'] = 0
+
+            #Temporary variable
+            temp = DaTaFrAmE2['Roll No'][i]
+
+            # Counting attendance according to the given criteria and storing in respective columns.
+            for j in DaTaFrAmE1.index:
+                if (temp == DaTaFrAmE1['Roll'][j]):
+                    if (DaTaFrAmE1['Date'][j] != -1):
+                        ind = VAL_DATES.index(DaTaFrAmE1['Date'][j])
+                        DaTaFrAmE3.at[ind+1, 'Total Attendance Count'] += 1
+                        if (DaTaFrAmE1['Time'][j] >= '14:00' and DaTaFrAmE1['Time'][j] <= '15:00'):
+                            if (DaTaFrAmE3['Real'][ind+1] == 0):
+                                DaTaFrAmE3.at[ind+1, 'Real'] += 1
+                            else:
+                                DaTaFrAmE3.at[ind+1, 'Duplicate'] += 1
+                        else:
+                            DaTaFrAmE3.at[ind+1, 'Invalid'] += 1
+
+            # Marking absent from the above evaluated attendance.
+            for j in range(1, len(VAL_DATES)+1):
+                if (DaTaFrAmE3['Real'][j] == 0):
+                    DaTaFrAmE3.at[j, 'Absent'] = 1
+                    FIN_DaTaFrAmE.at[i+1, VAL_DATES[j-1]] = 'A'
+                else:
+                    FIN_DaTaFrAmE.at[i+1, VAL_DATES[j-1]] = 'P'
+                    FIN_DaTaFrAmE.at[i+1, 'Total Real'] += 1
+                FIN_DaTaFrAmE.at[i+1, '% Attendance'] = round(
+                    FIN_DaTaFrAmE.at[i+1, 'Total Real']*100/FIN_DaTaFrAmE.at[i+1, 'Actual Lecture Taken'], 2)
+
+            # Saving the excel file for each roll number.
+            DaTaFrAmE3.to_excel('output/'+temp+'.xlsx', index=False)
+    except:
+        print("Index overflow, check the range again.")
