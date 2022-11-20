7189a2c39c074506ae7d6c8cb542d66cf6af0e7b
Mukul <panchalmukul99@gmail.com>
2022-11-20 19:37:17 +0530

    Here we go again.
    Starting the code.

---
+++ tut06/tut06.py
@@ -0,0 +1,9 @@
+#Mukul 2001CE35
+#Importing pandas.
+
+import pandas as pAnDa_jI
+
+from datetime import datetime
+start_time = datetime.now()
+
+#In the below code we will be reading and writing in multiple .xlsx files.
