2277bb34ab19e0a2b24b4dfc7c9fdc5dc6f0e084
Mukul <panchalmukul99@gmail.com>
2022-11-20 19:39:00 +0530

    Created a new function.
    Reading the given files, and adding empty columns for processing the given data.

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -7,3 +7,18 @@ from datetime import datetime
 start_time = datetime.now()
 
 #In the below code we will be reading and writing in multiple .xlsx files.
+
+def AtTeNdAnCe_ReP():
+    try:
+        #Reading the input .csv file and storing it in a variable.
+        DaTaFrAmE1=pAnDa_jI.read_csv('input_attendance.csv')
+        DaTaFrAmE2=pAnDa_jI.read_csv('input_registered_students.csv')
+    except:
+        print("There was an error reading the file.")
+    #Adding empty columns for processesing the given data.
+    DaTaFrAmE1['Roll']=''
+    DaTaFrAmE1['Time']=''
+    DaTaFrAmE1['Date']=''
+    DaTaFrAmE1['Day']=''
+    DaTaFrAmE1['Month']=''
+    DaTaFrAmE1['Year']=''
